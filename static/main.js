0e012cc5c5e468109c6257591dab93b17b428c8a
Viserest <50747852+Viserest@users.noreply.github.com>
2024-08-23 23:11:58 -0500

    Debugged

--- static/main.js
+++ static/main.js
@@ -2,6 +2,7 @@ window.onload = () => {
     main_slide = 0;
     slides = document.getElementById('mainslide').children;
     setInterval(()=>{
+        console.log(main_slide);
         slides[main_slide].style.display = "none";
         main_slide++;
         slides[main_slide].style.display = "block";
