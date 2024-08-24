2faf4446b182e9e7a7d291a33ee07e0b3bf9860c
Viserest <50747852+Viserest@users.noreply.github.com>
2024-08-23 23:03:33 -0500

    Updated slideshow javascript

--- static/main.js
+++ static/main.js
@@ -2,9 +2,9 @@ window.onload = () => {
     main_slide = 0;
     slides = document.getElementById('mainslide').children;
     setInterval(()=>{
-        slides[main_slide].styles.display = "none";
+        slides[main_slide].style.display = "none";
         main_slide++;
-        slides[main_slide].styles.display = "block";
+        slides[main_slide].style.display = "block";
     },5000);
 }
 
